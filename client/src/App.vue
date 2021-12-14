4f9dd50804ff358f28ff4562821316e832015b5a
Yumyummyyy <jamestiah@icloud.com>
2021-12-14 17:09:55 +0800

    to fix vue toolbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,42 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <v-card
+    color="grey lighten-4"
+    flat
+    height="200px"
+    tile
+  >
+    <v-toolbar dense>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Login from './components/Login'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Login
+  }
 }
 </script>
 
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vuetify from 'vuetify'
+import Homepage from '@/components/Homepage'
+import Login from '@/components/Login'
 
 Vue.use(Router)
+Vue.use(Vuetify)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Homepage',
+      component: Homepage
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
