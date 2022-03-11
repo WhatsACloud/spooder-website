7949d4ca0085313b6307b496210f2b10813402c2
Yumyummyyy <91830137+Yumyummyyy@users.noreply.github.com>
2022-03-11 22:58:15 +0800

    The navigation bar is relatively ok now

--- client/src/components/navBar/navBar.js
+++ client/src/components/navBar/navBar.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from '../../scss/navBar';
+import styles from '../../scss/navBar.module';
 
 const navBar = () => {
   return (
-    <nav className={styles.nav}>
-      <ul>
-        <li className="nav-item">
-          <a href="/about">test</a>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <nav className={styles.nav}>
+        <ul>
+          <li><a className={styles.logo} href="/">testing</a></li>
+          <li className={styles.normal}><a href="/about">about</a></li>
+          <li className={styles.normal}><a href="/">test</a></li>
+          <li className={styles.normal}><a href="/about">test</a></li>
+          <li className={styles.normal}><a href="/about">test</a></li>
+          <li className={styles.normal}><a href="/about">test</a></li>
+          <li className={styles.normal}><a href="/about">test</a></li>
+          <li className={styles.normal}><a href="/about">test</a></li>
+          <li className={styles.normal}><a href="/about">test</a></li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
