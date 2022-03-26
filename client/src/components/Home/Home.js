7fb83e1c04cf487a43acdddfd900e4d8bf1edc8e
Yumyummyyy <91830137+Yumyummyyy@users.noreply.github.com>
2022-03-26 15:49:53 +0800

    trying to fix issue with context menu where toggling it on a button triggers mouseleave event.

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -31,7 +31,7 @@ function RenderSpoodawebPreviews(props) {
     'another test': 'https://thumbs.lucid.app/documents/thumb/f95d1759-4be2-4fee-847a-be2037ca4926/0/163/NULL/140?clipToPage=false'
   }
   const spoodawebPreviews = Object.keys(spoodawebs).map((spoodaweb) => (
-    <button className={styles.spoodawebButton}>
+    <button className={styles.spoodawebButton} onMouseEnter={() => props.setCanShow(true)} onMouseLeave={props.setCanShow(false)}>
       <div className={styles.image}>
         <img src={spoodawebs[spoodaweb]}></img>
       </div>
@@ -46,7 +46,6 @@ function RenderSpoodawebPreviews(props) {
 }
 
 const ContextMenu = (props) => {
-  console.log('a')
   if (props.show) {
     return (
       <div style={{'top': props.y, 'left': props.x}} className={styles.contextMenu}>
@@ -65,27 +64,51 @@ const ContextMenu = (props) => {
   }
 }
 
-const Home = () => { // todo: figure out how to stop this thing from rerendering so can stop needlessly hitting backend authorization
-  const [ anchorPoint, setAnchorPoint ] = useState({ x: 0, y: 0})
-  const [ show, setShow ] = useState(false)
-  const handleContextMenu = (e) => {
+const handleContextMenu = (e, setAnchorPoint, canShow, setShow) => {
+  if (canShow) {
     setAnchorPoint({ x: e.pageX, y: e.pageY })
     setShow(true)
+    
     e.preventDefault()
+  } else {
+    setShow(false)
   }
+}
+
+const handleClick = (setShow) => {
+  setShow(false)
+  console.log('show: false')
+}
+
+const Home = () => {
+  const [ anchorPoint, setAnchorPoint ] = useState({ x: 0, y: 0})
+  const [ show, setShow ] = useState(false) // directly affect whether component can display or not
+  const [ canShow, setCanShow ] = useState(false) // checks if mouse hovering over button, if so then can display
   const navigate = useNavigate()
 
+  const handleContextMenuWrapper = e => {
+    handleContextMenu(e, setAnchorPoint, canShow, setShow)
+    console.log(`show: ${show}`, `canShow: ${canShow}`)
+  }
+
+  const handleClickWrapper = e => {
+    handleClick(setShow)
+    console.log(`show: ${show}`, `canShow: ${canShow}`)
+  }
+
   useEffect(() => {
-    document.addEventListener("contextmenu", handleContextMenu);
+    document.addEventListener("contextmenu", handleContextMenuWrapper);
+    document.addEventListener("click", handleClickWrapper);
     return (() => {
-      document.removeEventListener("contextmenu", handleContextMenu);
+      document.removeEventListener("contextmenu", handleContextMenuWrapper);
+      document.removeEventListener("click", handleClickWrapper);
     })
   })
   return (
     <>
       <Authorizer navigate={navigate}></Authorizer>
       <ContextMenu x={anchorPoint.x} y={anchorPoint.y} show={show}></ContextMenu>
-      <RenderSpoodawebPreviews></RenderSpoodawebPreviews>
+      <RenderSpoodawebPreviews setCanShow={setCanShow}></RenderSpoodawebPreviews>
     </>
   );
 };
