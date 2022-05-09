ba70ea14b3f51c348a0cd83fc9a0948ba76a7af4
Yumyummyyy <91830137+Yumyummyyy@users.noreply.github.com>
2022-05-09 21:23:12 +0800

    get function now works

--- server/models/bud.js
+++ server/models/bud.js
@@ -74,7 +74,6 @@ async function createAttachedTo(attachedToId, attachedTo, fk_bud_id, transaction
 }
 
 async function createBudDetails(budId, definition, sound, context, link, transaction) {
-  console.log(budId, definition, sound, context)
   const budDetails = await BudDetails.create({
     fk_bud_id: budId,
     definition: definition,
@@ -82,7 +81,6 @@ async function createBudDetails(budId, definition, sound, context, link, transac
     link: link,
     context: context
   }, {transaction: transaction})
-  console.log(budDetails)
   return budDetails
 }
 
@@ -145,7 +143,7 @@ async function editExample(contextId, example, transaction) {
   }, {transaction: transaction})
 }
 
-async function getObjsWithinRange(spoodawebId, startPos, endPos) {
+async function getBudsWithinRange(spoodawebId, startPos, endPos) {
   console.log(startPos, endPos)
   const objs = await Bud.findAll({
     where: {
@@ -158,6 +156,47 @@ async function getObjsWithinRange(spoodawebId, startPos, endPos) {
       y: {
         [Op.and]: {
           [Op.gt]: startPos[1],
+          [Op.lt]: endPos[1],
+        }
+      },
+      fk_spoodaweb_id: spoodawebId
+    }
+  }) 
+  return objs
+}
+
+async function editExample(contextId, example, transaction) {
+  const _example = await Example.findOne({ where: { fk_context_id: contextId }})
+  if (_example === null) return null
+  await _example.update({
+    example: example
+  }, {transaction: transaction})
+}
+
+async function getSilksWithinRange(spoodawebId, startPos, endPos) {
+  const objs = await Silk.findAll({
+    where: {
+      x1: {
+        [Op.and]: {
+          [Op.gt]: startPos[0],
+          [Op.lt]: endPos[0],
+        }
+      },
+      y1: {
+        [Op.and]: {
+          [Op.gt]: startPos[0],
+          [Op.lt]: endPos[1],
+        }
+      },
+      x2: {
+        [Op.and]: {
+          [Op.gt]: startPos[0],
+          [Op.lt]: endPos[0],
+        }
+      },
+      y2: {
+        [Op.and]: {
+          [Op.gt]: startPos[0],
           [Op.lt]: endPos[1],
         }
       },
@@ -210,52 +249,79 @@ module.exports = { // please add support for positions, budId
       const startPos = req.body.startPos
       const endPos = req.body.endPos
       const user = (await getUser(req.body.jwtTokenData.userId))
-      console.log(user)
       if (!user) return error.create('user does not exist')
       const userId = user.dataValues.id
       const spoodaweb = await getSpoodaweb(req.body.spoodawebId)
       if (spoodaweb === null) return error.create('spoodaweb does not exist')
       if (!(userId === spoodaweb.dataValues.fk_user_id)) return error.create('requested spoodaweb is not under user')
       const spoodawebId = spoodaweb.dataValues.id
-      const dbObjs = await getObjsWithinRange(spoodawebId, startPos, endPos)
-      for (const dbObj of dbObjs) {
+      const dbBudObjs = await getBudsWithinRange(spoodawebId, startPos, endPos)
+      for (const dbObj of dbBudObjs) {
         const objData = dbObj.dataValues
         const objId = objData.objId
-        switch (objData.type) {
-          case "bud":
-            toResObjs[objId] = {
-              word: objData.word,
-              definitions: [],
-              position: {
-                x: objData.x,
-                y: objData.y
-              },
-              attachedTo: {}, 
-              type: "bud"
-            }
-            const budDetails = await getBudDetails(objData.id)
-            let i = 0
-            for (const budDetail of budDetails) {
-              const budDetailData = budDetail.dataValues
-              const budDetailId = budDetailData.id
-              toResObjs[objId].definitions[i].sound.push(budDetailData.sound)
-              toResObjs[objId].definitions[i].definition.push(budDetailData.definition)
-              toResObjs[objId].definitions[i].context.push(budDetailData.context)
-              const examplesObj = []
-              const examples = await getExamples(budDetailId)
-              for (const example of examples) {
-                examplesObj.push(example.dataValues.example)
-              }
-              toResObjs[objId].definitions[i].examples.push(examplesObj)
-              i++
-            }
-            const attachedTos = await getAttachedTos(objData.id)
-            for (const attachedTo of attachedTos) {
-              const attachedToData = attachedTo.dataValues
-              toResObjs.attachedTo[attachedToData.attachedToId] = attachedToData.attachedTo
-            }
-          }
+        toResObjs[objId] = {
+          word: objData.word,
+          definitions: [],
+          position: {
+            x: objData.x,
+            y: objData.y
+          },
+          attachedTo: {}, 
+          type: "bud"
         }
+        const budDetails = await getBudDetails(objData.id)
+        let i = 0
+        for (const budDetail of budDetails) {
+          const budDetailData = budDetail.dataValues
+          const budDetailId = budDetailData.id
+          console.log(toResObjs[objId])
+          toResObjs[objId].definitions.push({
+            sound: null,
+            definition: null,
+            context: null,
+            examples: [] 
+          })
+          toResObjs[objId].definitions[i].sound = budDetailData.sound
+          toResObjs[objId].definitions[i].definition = budDetailData.definition
+          toResObjs[objId].definitions[i].context = budDetailData.context
+          const examplesObj = []
+          const examples = await getExamples(budDetailId)
+          for (const example of examples) {
+            examplesObj.push(example.dataValues.example)
+          }
+          toResObjs[objId].definitions[i].examples.push(examplesObj)
+          i++
+        }
+        const attachedTos = await AttachedTo.findAll({
+          where: {
+            fk_bud_id: objData.id
+          }
+        }) 
+        for (const attachedTo of attachedTos) {
+          const attachedToData = attachedTo.dataValues
+          toResObjs[objId].attachedTo[attachedToData.attachedToId] = attachedToData.attachedTo
+        }
+      }
+      const dbSilkObjs = await getSilksWithinRange(spoodawebId, startPos, endPos)
+      console.log(dbSilkObjs)
+      for (const silk of dbSilkObjs) {
+        const silkData = silk.dataValues
+        const objId = silkData.objId
+        toResObjs[objId] = {
+          "positions": [],
+          "strength": null,
+          "attachedTo1": null,
+          "attachedTo2": null,
+          "type": "silk"
+        }
+        toResObjs[objId].positions = [
+          {x: silkData.x1, y: silkData.y1},
+          {x: silkData.x2, y: silkData.y2}
+        ]
+        toResObjs[objId].strength = silkData.strength
+        toResObjs[objId].attachedTo1 = silkData.attachedTo1
+        toResObjs[objId].attachedTo2 = silkData.attachedTo2
+      }
       req.body.spoodawebData = toResObjs
       next()
     } catch (err) {
@@ -286,13 +352,11 @@ module.exports = { // please add support for positions, budId
                   const budDetailsId = _budDetailsId.dataValues.id
                   const examples = definition.examples
                   for (const example of examples) {
-                    console.log("example", example)
                     await createExample(budDetailsId, example, transaction)
                   }
                 }
                 let i = 0
                 for (const [ attachedToId, attachedTo ] of Object.entries(obj.attachedTo)) {
-                  console.log(attachedToId, attachedTo)
                   await createAttachedTo(attachedToId, attachedTo, budId, transaction)
                   i++
                 }
