3dd5137c6ada45e285614cf6f983c9ac8c3ddfb0
Yumyummyyy <jamestiah@icloud.com>
2022-05-12 18:55:57 +0800

    did something on pld 2

--- client/src/components/Edit/Select/BudView.js
+++ client/src/components/Edit/Select/BudView.js
@@ -2,23 +2,18 @@ import React, { useEffect, useState } from 'react'
 import styles from './select.module'
 import * as utils from '../utils'
 
-function BudAttr({ id, placeholder, style }) {
+function BudAttr({ id, placeholder, style, data }) {
   return (
     <div
       className={styles.attrBox}
       id={id}>
       <input
+        value={data}
         className={style || styles.normal}
         placeholder={placeholder}>
       </input>
     </div>
   ) 
-}
-
-function example() {
-  return (
-
-  )
 }
 
 const budSample = {
@@ -45,15 +40,26 @@ function BudView({ selectedObj }) {
   const [ obj, setObj ] = useState(budSample)
   const [ definitionNo, setDefinitionNo ] = useState(0)
   const [ renderedExamples, setRenderedExamples ] = useState()
+  const [ renderData, setRenderData ] = useState()
   useEffect(() => {
     const actualObj = utils.getObjById(selectedObj)
     setObj(budSample)
     console.log(actualObj)
+    const currentDefinitionObj = obj.definitions[definitionNo]
+    const data = {
+      word: obj.name,
+      definition: currentDefinitionObj.definition,
+      sound: currentDefinitionObj.sound,
+      context: currentDefinitionObj.context,
+      link: currentDefinitionObj.link
+    }
+    setRenderData(data)
   }, [ selectedObj ])
   return (
     <div className={obj ? styles.BudView : styles.none} id='BudView'>
       <BudAttr
         id='word'
+        data={renderData ? renderData.word: null}
         placeholder='insert word'></BudAttr>
       <BudAttr
         id='definition'
