2781200ea62993688d54bd6a9d546dc35f70bb04
Yumyummyyy <jamestiah@icloud.com>
2022-05-12 18:40:43 +0800

    did something on pld

--- client/src/components/Edit/Select/BudView.js
+++ client/src/components/Edit/Select/BudView.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react'
-import styles from '../edit.module'
+import React, { useEffect, useState } from 'react'
+import styles from './select.module'
+import * as utils from '../utils'
 
 function BudAttr({ id, placeholder, style }) {
   return (
@@ -14,9 +15,43 @@ function BudAttr({ id, placeholder, style }) {
   ) 
 }
 
-function BudView({ selectedObj }) {
+function example() {
   return (
-    <div className={selectedObj ? styles.BudView : styles.none} id='BudView'>
+
+  )
+}
+
+const budSample = {
+  "name": "gay",
+  "definitions": [
+    {
+      "definition": "definition 1",
+      "sound": "sound 1",
+      "context": "context 1",
+      "examples": [
+        "example 1",
+        "example 2"
+      ],
+      "link": 0.2 
+    }
+  ],
+  "attachedTo": [],
+  "position": {},
+  "type": "bud", // bud silk
+  "operation": 'add' // add edit minus, default add,
+}
+
+function BudView({ selectedObj }) {
+  const [ obj, setObj ] = useState(budSample)
+  const [ definitionNo, setDefinitionNo ] = useState(0)
+  const [ renderedExamples, setRenderedExamples ] = useState()
+  useEffect(() => {
+    const actualObj = utils.getObjById(selectedObj)
+    setObj(budSample)
+    console.log(actualObj)
+  }, [ selectedObj ])
+  return (
+    <div className={obj ? styles.BudView : styles.none} id='BudView'>
       <BudAttr
         id='word'
         placeholder='insert word'></BudAttr>
@@ -24,15 +59,21 @@ function BudView({ selectedObj }) {
         id='definition'
         placeholder='definition'
         style={styles.definition}></BudAttr>
-      <div>
-        <BudAttr
-          id='sound'
-          placeholder='sound'
-          style={styles.sound}></BudAttr>
-        <BudAttr
-          id='context'
+      <BudAttr
+        id='sound'
+        placeholder='sound'
+        style={styles.sound}></BudAttr>
+      <div
+        id='context'
+        className={styles.attrBox}>
+        <textarea
           placeholder='context'
-          style={styles.context}></BudAttr>
+          className={styles.context}></textarea>
+      </div>
+      <div
+        id='examples'
+        className={styles.examplesBox}>
+        {renderedExamples}
       </div>
     </div>
   )
