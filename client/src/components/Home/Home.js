4a3c8924c64a8870c23ae32bb87627e685f63848
Yumyummyyy <jamestiah@icloud.com>
2022-05-15 22:39:19 +0800

    just go to sleep james

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -11,8 +11,6 @@ import Authorizer from '../Shared/Authorizer'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
-
-
 const spoodawebSchema = object({
   title: string().required('Title is a required field')
 })
