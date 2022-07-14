924757bd49d5c7ba34f4fc815035418facb632ba
Yumyummyyy <jamestiah@icloud.com>
2022-07-14 22:12:58 +0800

    making some visual changes

--- client/src/components/Edit/TaskBar/TaskBar.js
+++ client/src/components/Edit/TaskBar/TaskBar.js
@@ -3,7 +3,7 @@ import styles from './taskBar.module'
 import api from '../../../services/api'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGear, faFileLines } from '@fortawesome/free-solid-svg-icons'
+import { faGear } from '@fortawesome/free-solid-svg-icons'
 
 import { SearchBar, SearchResult } from './Search'
 import { UndoRedo } from './UndoRedo'
@@ -13,25 +13,21 @@ import { Debugger } from './debug'
 import * as utils from '../utils'
 
 function TaskBar({ setInSettings }) {
+  const [ hover, setHover ] = useState(false)
   return (
     <>
       <div className={styles.sideBar}>
 
       </div>
       <div className={styles.taskBar}>
-        <button
-          className={styles.stdButton}
-          onClick={() => setInSettings(true)}>
-          Settings
-        </button>
-        <button
+        {/* <button
           className={styles.stdButton}
           onClick={() => {
             const modes = utils.getGlobals().modes
             modes.autoDrag = !(modes.autoDrag)
           }}>
           <FontAwesomeIcon icon={faFileLines}></FontAwesomeIcon>
-        </button>
+        </button> */}
         <button
           className={styles.stdButton}
           onClick={() => {
@@ -40,10 +36,19 @@ function TaskBar({ setInSettings }) {
           }}>
           glue
         </button>
-        <SearchBar></SearchBar>
-        <Categories></Categories>
+        {/* <SearchBar></SearchBar>
+        <Categories></Categories> */}
         {/* <Debugger></Debugger> */}
-        <UndoRedo></UndoRedo>
+        {/* <UndoRedo></UndoRedo> */}
+        <button
+          className={styles.stdButton}
+          onClick={() => setInSettings(true)}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          >
+          <FontAwesomeIcon className={hover ? styles.on : ''} icon={faGear}></FontAwesomeIcon>
+          <p>Settings</p>
+        </button>
       </div>
     </>
   )
