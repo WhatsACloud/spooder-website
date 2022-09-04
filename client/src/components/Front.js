01615fd38e7a0eb7308bcedc8dd2dd5505dc5361
Yumyummyyy <jamestiah@icloud.com>
2022-09-04 12:30:16 +0800

    fixed some bugs

--- client/src/components/Front.js
+++ client/src/components/Front.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Authorizer from './Shared/Authorizer'
+import NavBar from './navBar'
 
 const Front = () => {
   const navigate = useNavigate()
   return (
     <>
+      <NavBar></NavBar>
       <Authorizer navigate={navigate}></Authorizer>
     </>
   );
--- client/src/components/user/register.js
+++ client/src/components/user/register.js
@@ -34,7 +34,7 @@ async function signUp(errorStates, changeErrorState, changeServerErrorState, nav
   }
 }
 
-const Register = ({ setInLogin }) => {
+const Register = () => {
   let navigate = useNavigate()
   let [ errorStates, changeErrorState ] = useState({
     "Username": false,
